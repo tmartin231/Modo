3d5de3a135cfc92d8cf6c95591215bdb5cc108d7
tmartin231 <tmartin231@outlook.de>
2026-03-04 18:45:01 +0100

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./i18n.ts";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
